5b9148c67188fc4b10e0e21358e1f0ea95990115
Harry Parsons <hparsons93@gmail.com>
2021-09-09 11:59:17 +0100

    spec error

--- spec/features/seeing_hit_points_spec.rb
+++ spec/features/seeing_hit_points_spec.rb
@@ -4,7 +4,7 @@ feature 'Seeing Hit Points' do
     fill_in :player_1_name, with: 'Harry'
     fill_in :player_2_name, with: 'Ranald'
     click_button 'Submit'
-    expect(page).to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Ranald: 60HP'
   end 
 end 
 
