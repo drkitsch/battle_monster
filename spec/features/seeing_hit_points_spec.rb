411ab63834b39b89c3eec46a354aa18a17b0ffa7
Harry Parsons <hparsons93@gmail.com>
2021-09-09 12:19:02 +0100

    Added web helper

--- spec/features/seeing_hit_points_spec.rb
+++ spec/features/seeing_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Seeing Hit Points' do 
   scenario 'Seeing P2 Hit Points' do 
-    visit('/') 
-    fill_in :player_1_name, with: 'Harry'
-    fill_in :player_2_name, with: 'Ranald'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ranald: 60HP'
   end 
 end 
